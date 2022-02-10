093ea930e00d5795378c0b76793e5d452dbef42b
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-02-10 17:23:30 +0600

    Created navbar component

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import "./App.css";
+import Navbar from "./components/Navbar";
 export default function App() {
+  const title = "Welcome to My Blog";
+  const likes = 10;
   return (
-    <div>
-      <h1> Blog site </h1>{" "}
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <h1> {title}</h1> <p>Liked {likes} times</p>
+      </div>
     </div>
   );
 }

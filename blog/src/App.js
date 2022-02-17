9a1b35ae135bda1ed48e752eb5e94d26e6a296b8
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-02-17 17:23:15 +0600

    Router links

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Create from "./components/Create";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 export default function App() {
   return (
     <Router>
@@ -9,8 +11,11 @@ export default function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
             </Route>
           </Switch>
         </div>

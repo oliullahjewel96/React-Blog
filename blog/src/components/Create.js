b7254f6d999b7e7d54f516a4203d3008ece00b48
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-02-18 17:21:20 +0600

    Programmic redirects

--- blog/src/components/Create.js
+++ blog/src/components/Create.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useHistory } from "react-router";
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("Mario");
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,6 +18,7 @@ const Create = () => {
     }).then(() => {
       console.log("New blog added");
       setIsPending(false);
+      history.push("/");
     });
   };
   return (

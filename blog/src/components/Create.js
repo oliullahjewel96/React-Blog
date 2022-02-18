190ec9521a598a8cb11ccf2bbebe4a5803057cfc
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-02-18 16:26:36 +0600

    Make a POST request to the json server

--- blog/src/components/Create.js
+++ blog/src/components/Create.js
@@ -3,10 +3,25 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("Mario");
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    setIsPending(true);
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("New blog added");
+      setIsPending(false);
+    });
+  };
   return (
     <div className="create">
       <h1> Add a new Blog </h1>{" "}
-      <form>
+      <form onSubmit={handleSubmit}>
         <label> Blog title: </label>
         <input
           type="text"
@@ -25,7 +40,8 @@ const Create = () => {
           <option value="Mario">Mario </option>{" "}
           <option value="Yoshi"> Yoshi</option>{" "}
         </select>{" "}
-        <button> Add Blog </button>
+        {!isPending && <button> Add Blog </button>}
+        {isPending && <button> Adding Blog.... </button>}
       </form>{" "}
     </div>
   );

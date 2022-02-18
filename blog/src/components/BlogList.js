4e0edebd7e50bed7005832e6f827b1bdfee6e278
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-02-18 15:31:36 +0600

    Modified route link

--- blog/src/components/BlogList.js
+++ blog/src/components/BlogList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 const BlogList = (props) => {
   const { blogs, title } = props;
 
@@ -7,9 +8,11 @@ const BlogList = (props) => {
       <h2> {title} </h2>{" "}
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2> {blog.title} </h2> <p> Written by {blog.author} </p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2> {blog.title} </h2> <p> Written by {blog.author} </p>
+          </Link>{" "}
         </div>
-      ))}
+      ))}{" "}
     </div>
   );
 };
